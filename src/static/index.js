a9507b63dc66f034ad4ce0625811f9f9d7fae686
hewei <hewei95324@offcn.com>
2020-06-03 11:04:46 +0800

    删除冗余图片

--- src/static/index.js
+++ src/static/index.js
@@ -1,9 +1,5 @@
 import person from './images/mine/person.png';
-import phone from './images/mine/phone.png';
 import order from './images/mine/order.png';
 import order_wait from './images/mine/order_wait.png';
-import post from './images/post.png';
-import code from './images/code.jpg';
-import star from './images/star.png';
 
-export { person, star, order, order_wait, post, code };
+export { person,  order, order_wait};
